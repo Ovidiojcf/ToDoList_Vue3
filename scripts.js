cdd1f5004dc5d49d9bd6136c96adf83dbaaa0274
Ovidiojcf <ojcfarias@gmail.com>
2025-02-25 16:10:06 -0300

    implemented lifeCycleHooks

--- scripts.js
+++ scripts.js
@@ -29,12 +29,23 @@ const toDoApp = {
                 this.tasks.push({...this.newTask}); //adiciona no array do usuário
                 this.newTask = {
                     done: false
-                }
+                };
+                localStorage.setItem('tasks', JSON.stringify(this.tasks));
             }else{
                 alert('To-do text is required')
             }
+        },
+        storeAll(){
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
         }
+    },
+    created(){
+        this.tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : this.tasks;
     }
+    //necessário para atualizar a tarefa como feito no localStorage
+    // updated(){
+    //     localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    // }
 };
 
 Vue.createApp(toDoApp).mount('#app');
